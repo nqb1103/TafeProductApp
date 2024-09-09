c7456d58856ee10a0bccbfe09d46cb7e12c667a7
001202394 <Quoc.NGUYEN25@student.tafesa.edu.au>
2024-09-09 14:49:55 +0930

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -35,6 +35,7 @@ namespace ProductApps
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25.00m);
+                totalChargeWrapTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25.00m + 5.00m);
             }
             catch (FormatException)
             {
